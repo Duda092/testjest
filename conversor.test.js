22244a2c9c4c04162bbb64ac535149fb2735c82d
Duda092 <102489310+Duda092@users.noreply.github.com>
2024-02-28 12:41:01 -0300

    Add files via upload

---
+++ conversor.test.js
@@ -0,0 +1,8 @@
+const cacular = require(./index);
+
+describe('Testa a função Conversor', ()=>{
+    test('Verifica se 40 em fahrenheit é 104')
+})
+
+const conversor = 104
+expect(conversor).toBe(104;)
---
+++ index.js
@@ -0,0 +1,8 @@
+const {moduleExpression} = require("@babel/types");
+
+function ConversorDeTemperatura {
+    const conversor = (celsius * 1.8) + 32
+    return conversor
+}
+
+module.exports = cacular;
